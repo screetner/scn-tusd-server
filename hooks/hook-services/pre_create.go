f4b04712c0c2976ae1018dd9a2580c5244086300
Quercussi <somsit.thanapat@gmail.com>
2024-09-08 19:32:56 +0700

    add: template for setting upload directory on Azure blob

--- hooks/hook-services/pre_create.go
+++ hooks/hook-services/pre_create.go
@@ -3,6 +3,7 @@ package hook_services
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
 	"github.com/tus/tusd/v2/pkg/hooks"
 	"io"
 	"log"
@@ -11,25 +12,51 @@ import (
 func PreCreateHookHandler(req hooks.HookRequest) (res hooks.HookResponse, err error) {
 	res.HTTPResponse.Header = make(map[string]string)
 
-	if filename, ok := req.Event.Upload.MetaData["filename"]; !ok {
-		res.RejectUpload = true
-		res.HTTPResponse.StatusCode = 400
-		res.HTTPResponse.Body = "no filename provided"
-		res.HTTPResponse.Header["X-Some-Header"] = "yes"
-	} else {
-
-		if res.ChangeFileInfo.Storage == nil {
-			res.ChangeFileInfo.Storage = make(map[string]string)
-		}
-
-		directoryName := "records"
-		id := directoryName + "/" + uid()
-
-		res.ChangeFileInfo.ID = id
-		log.Printf("PRE_CREATE: Uploading %s at : %s\n", filename, id)
+	fileName, fileNameErr := validateMetaDataField(&res, &req, "filename")
+	if fileNameErr != nil {
+		return res, fileNameErr
 	}
 
+	sessionName, sessionNameErr := validateMetaDataField(&res, &req, "sessionName")
+	if sessionNameErr != nil {
+		return res, sessionNameErr
+	}
+
+	uniqueSessionId := uid()
+
+	if res.ChangeFileInfo.MetaData == nil {
+		res.ChangeFileInfo.MetaData = make(map[string]string)
+	}
+
+	// TODO: validate user upload permission
+
+	// TODO: decode organizationId from jwt token
+	organizationId := "orgId1"
+
+	// TODO: decode userId from jwt token
+	userId := uid()
+
+	directoryName := organizationId
+	id := directoryName + "/records/" + sessionName + "_" + uniqueSessionId + "/" + fileName
+
+	res.ChangeFileInfo.ID = id
+	res.ChangeFileInfo.MetaData["recorderUserId"] = userId
+	log.Printf("PRE_CREATE: Uploading %s at : %s\n", fileName, id)
+
 	return res, nil
+}
+
+func validateMetaDataField(res *hooks.HookResponse, req *hooks.HookRequest, fieldName string) (string, error) {
+	fieldValue, ok := req.Event.Upload.MetaData[fieldName]
+	if !ok {
+		errorMessage := fmt.Sprintf("no %s provided in the request metadata", fieldName)
+		res.RejectUpload = true
+		res.HTTPResponse.StatusCode = 400
+		res.HTTPResponse.Body = errorMessage
+		res.HTTPResponse.Header["X-Some-Header"] = "yes"
+		return "", fmt.Errorf(errorMessage)
+	}
+	return fieldValue, nil
 }
 
 // found in github.com/tus/tusd/v2/internal/hooks/uid
