8467f84cfb0398780f7902b80d228e76cdf39e83
Quercussi <somsit.thanapat@gmail.com>
2024-09-20 10:03:55 +0700

    add: specify upload path for each file

--- hooks/hook-services/pre_create.go
+++ hooks/hook-services/pre_create.go
@@ -1,12 +1,17 @@
 package hook_services
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/tus/tusd/v2/pkg/hooks"
 	"io"
 	"log"
+	"scn-tusd-server/hooks/types"
+	services "scn-tusd-server/services"
+	"strings"
 )
 
 func PreCreateHookHandler(req hooks.HookRequest) (res hooks.HookResponse, err error) {
@@ -28,20 +33,66 @@ func PreCreateHookHandler(req hooks.HookRequest) (res hooks.HookResponse, err er
 		res.ChangeFileInfo.MetaData = make(map[string]string)
 	}
 
+	headers := make(map[string]string)
+	for key, values := range req.Event.HTTPRequest.Header {
+		if len(values) > 0 {
+			headers[key] = values[0]
+		}
+	}
+	body := bytes.NewBuffer([]byte(`{}`))
+
+	backendResp, backedErr := services.GetAPIClient().Get("/tusd/user/info", headers, body)
+	if backedErr != nil {
+		// Handle the error
+		log.Printf("Error validating: %v", err)
+		return res, backedErr
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("Error closing response body: %v", err)
+		}
+	}(backendResp.Body)
+
+	var userInfo types.UserInfo // Use the UserInfo type from the types package
+	if err := json.NewDecoder(backendResp.Body).Decode(&userInfo); err != nil {
+		log.Printf("Error decoding JSON response: %v", err)
+		return res, err
+	}
+
+	if backendResp.StatusCode == 403 {
+		res.RejectUpload = true
+		res.HTTPResponse.StatusCode = 403
+		res.HTTPResponse.Body = "skibid"
+		return res, nil
+	}
+
 	// TODO: validate user upload permission
+	hasPermission := userInfo.AbilityScope.Mobile.Access
+	if !hasPermission {
+		res.RejectUpload = true
+		res.HTTPResponse.StatusCode = 403
+		res.HTTPResponse.Body = "User does not have permission to upload"
+		return res, nil
+	}
 
-	// TODO: decode organizationId from jwt token
-	organizationId := "orgId1"
+	organizationId := userInfo.OrgId
+	organizationName := userInfo.OrgName
 
-	// TODO: decode userId from jwt token
-	userId := uid()
+	userId := userInfo.UserId
+	username := userInfo.UserName
 
-	directoryName := organizationId
-	id := directoryName + "/records/" + sessionName + "_" + uniqueSessionId + "/" + fileName
+	organizationDirectory := fmt.Sprintf("%s_%s", organizationName, organizationId)
+	sessionDirectory := fmt.Sprintf("%s_%s", sessionName, uniqueSessionId)
+	id := fmt.Sprintf("%s/records/%s/%s", organizationDirectory, sessionDirectory, fileName)
+	id = strings.ReplaceAll(id, " ", "_")
 
 	res.ChangeFileInfo.ID = id
 	res.ChangeFileInfo.MetaData["recorderUserId"] = userId
+	res.ChangeFileInfo.MetaData["recorderUserName"] = username
 	log.Printf("PRE_CREATE: Uploading %s at : %s\n", fileName, id)
+
+	res.HTTPResponse.StatusCode = 200
 
 	return res, nil
 }
